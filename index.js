6fdf236a9e869a12d066621a30e59ded50967b8a
Mati Sediqi <mati_01@icloud.com>
2023-09-10 10:48:36 +0200

    feat: Add README and index.js files
    Added a new file named README with content providing an overview
    of the git-ai tool and its installation instructions. Also added a
    new file named index.js which includes the code for the main
    functionality of the tool. The code uses various packages such as
    simple-git, picocolors, dotenv, openai, and fs.
    
    The code involves the following steps:
    - Reading the .env file and validating the OPENAI_API_KEY
      environment variable.
    - Defining logging functions for console output.
    - Specifying a list of ignored files.
    - Checking the status of the git repository and filtering out the
      staged files.
    - If no staged files are found, an error is logged.
    - The code then iterates through each staged file, retrieves the
      diff content, and stores it in the gptMsg variable. If the diff
      content is empty, the entire file content is read and stored
      instead.
    - The intro message for the conventional-commits format is created.
    - The gptMsg and intro message are combined into the content
      variable.
    - The OpenAI API is utilized to send the content for chat
      completions.
    - The generated message from the AI model is logged to the console.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const simpleGit = require('simple-git')();
 const pc = require('picocolors');
 const dotenv = require('dotenv');
 const OpenAI = require('openai');
+const fs = require('fs');
 
 // .env stuff
 const { NODE_ENV } = process.env;
@@ -10,68 +11,74 @@ const { OPENAI_API_KEY } = process.env;
 
 // Logging
 const prefix = '[ git-ai ]';
-
-const log = msg => {
-    console.log(`${pc.yellow(prefix)} ${msg}`);
-};
-
-const error = msg => {
-    console.log(`${pc.red(prefix)} ${pc.red(msg)}`);
-};
+const log = (...msg) => console.log(`${pc.yellow(prefix)} ${msg.join('\n' + prefix + ' ')}`);
+const error = (...msg) => console.error(`${pc.red(prefix)} ${pc.red(msg.join('\n' + prefix + ' '))}`);
 
 // Validating key
 if ((OPENAI_API_KEY ?? '').length === 0) {
     return error('`OPENAI_API_KEY` environment variable is not set');
 }
 
+const ignoredFiles = [
+    'pnpm-lock.yaml',
+    'yarn.lock',
+    'package-lock.json',
+    'composer.lock',
+];
+
 // Actual magic
 (async () => {
-    const diffSummary = await simpleGit.diffSummary();
+    const statusSummary = await simpleGit.status();
+    const { staged } = statusSummary;
 
-    const ignoredFiles = [
-        'pnpm-lock.yaml',
-        'yarn.lock',
-        'package-lock.json',
-        'composer.lock',
-    ];
+    if (staged.length === 0) {
+        return error(
+            `repository's staged-area looks empty. No files to make a summary of.`,
+            `Use \`git add <file>\` to stage files.`,
+        );
+    }
 
-    const files = diffSummary.files.filter(file => {
-        const { file: filePath } = file;
+    const stagedFiles = statusSummary.files.filter(file => {
+        const { path: filePath } = file;
+
+        // We don't care about lock-files
+        if (ignoredFiles.some(extension => filePath.endsWith(extension))) {
+            return false;
+        }
+
         return !file.binary && !ignoredFiles.some(extension => filePath.endsWith(extension));
     });
 
-    if (files.length === 0) {
+    if (stagedFiles.length === 0) {
         return error('No files to commit');
     }
 
-    log(`Committing ${files.length} files...`);
+    log(`Committing ${stagedFiles.length} files...`);
 
     let gptMsg = '';
 
     // Loop through each file and get the diff content
-    for (const file of files) {
-        const { file: filePath } = file;
+    for (const file of stagedFiles) {
+        const { path: filePath } = file;
 
-        // We don't care about lock files
-        if (ignoredFiles.some(extension => filePath.endsWith(extension))) {
-            continue;
+        let diff = await simpleGit.diff([filePath]);
+        let diffMsg = `Diff content for file: ${filePath}:\n\n${diff}\n`;
+
+        if (diff.length === 0) {
+            diff = fs.readFileSync(filePath, 'utf-8');
+            diffMsg = `New file: ${filePath} with content:\n\n${diff}\n`;
         }
 
-        const diff = await simpleGit.diff([file.file]);
-        gptMsg += `Diff content for file: ${filePath}:\n\n${diff}\n`;
+        gptMsg += diffMsg;
     }
 
-    const intro = `Summarize my changes I'll provide you with. Use the following format of conventional-commits:
+    const intro = `Create a summary from the diff and explain what changes and why. Use the following format of conventional-commits:
 "<type>(optional scope): <Short description, up to 80 chars>
 \n
-Longer body/description. Hard-Wrap it to max. 80 chars per line. Be specific but not too detail-rich. Be descriptive.
+<descriptional text> - Wrap it to max. 80 chars per line. Be specific but not too detail-rich. Be descriptive.
 Use the imperative, present tense: "change" not "changed" nor "changes".
 Don't capitalize first letter of the description.
-No dot (.) at the end.
-
-"
-
-Flush the diff from your response. I do not want to see it.`;
+No dot (.) at the end."`;
     const content = intro + '\n\n' + gptMsg;
 
     const openai = new OpenAI({
