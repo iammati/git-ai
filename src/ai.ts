98a157ad6c96b9c1b34da7090c7fc5d010c7f0cc
Mati Sediqi <mati_01@icloud.com>
2023-09-10 15:35:01 +0200

    fix(build): Update build script to add changes and run the generated script (#10)
    Updating the build script in the package.json file to include the command `git add.`
    after running the `bun build` command. This will add all the changes to the Git
    repository before running the `bun run` command to execute the generated script.
    
    This change ensures that all the changes in the build directory are properly added
    to the Git repository before running the generated script.

--- src/ai.ts
+++ src/ai.ts
@@ -1,15 +1,13 @@
 import OpenAI from 'openai';
 import { OPENAI_API_KEY } from './environment';
 
-const intro = `Create a summary of the diff below the template, and explain what changes and why depending only on the actual diff. Use the following format of conventional-commits to summarize it:
+const intro = `Create a summary of the diff below the template, and explain what changes and why depending only on the actual diff. Wrap the description to 80 characters. Use the following format of conventional-commits to summarize it:
 "<type>(optional scope): <Short description, up to 80 chars>
 \n
 <descriptional text> - Be specific but not too detail-rich - be descriptive.
 Use the imperative, present tense: "change" not "changed" nor "changes".
 Don't capitalize first letter of the description.
-No dot (.) at the end."
-
-Please wrap every line inside the template to a max. of 80 characters. Wrap is mandatory!`;
+No dot (.) at the end."`;
 
 export const gitAi = async (diff: string) => {
     const openai = new OpenAI({
